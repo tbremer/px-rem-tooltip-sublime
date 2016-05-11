886c036d1e2114c7ca7e3ebd5021c6aa44f8d894
Tom Bremer <tom@tbremer.com>
2016-05-11 11:13:15 -0500

    regex as declared variables, allow for floats under zero

--- px_rem_tooltip.py
+++ px_rem_tooltip.py
@@ -5,25 +5,28 @@ class PXRem(sublime_plugin.EventListener):
 	def on_selection_modified(self, view):
 		syntax = view.settings().get('syntax')
 
+		SYNTAX_SEARCH = '(?:s|l)?(?:a|c|e)ss'
+		VALID_INPUT_SEARCH = '\.?\d+(?:px|rem)'
+		PX_SEARCH = 'px'
+		DIGIT_SEARCH = '(\.?\d+)'
+
 		popup_numbers = []
 
-		if re.search('(?:s|l)?(?:a|c|e)ss', syntax, re.IGNORECASE) is None:
+		if re.search(SYNTAX_SEARCH, syntax, re.IGNORECASE) is None:
 			return
 
 		cur_line_text = view.substr(view.line(view.sel()[0]))
 
-		for match in re.findall('\d+(?:px|rem)', cur_line_text):
-			type_match = re.search('px', match)
-
-			print(match)
+		for match in re.findall(VALID_INPUT_SEARCH, cur_line_text):
+			type_match = re.search(PX_SEARCH, match, re.IGNORECASE)
 
 			if type_match:
-				inner_match = re.search('(\d+)', match)
-				number = int(float(inner_match.group())) / 16
+				inner_match = re.search(DIGIT_SEARCH, match)
+				number = float(inner_match.group()) / 16
 				string = str(number) + 'rem;'
 			else:
-				inner_match = re.search('(\d+)', match)
-				number = int(float(inner_match.group())) * 16
+				inner_match = re.search(DIGIT_SEARCH, match)
+				number = float(inner_match.group()) * 16
 				string = str(number) + 'px;'
 
 			popup_numbers.append(string)
